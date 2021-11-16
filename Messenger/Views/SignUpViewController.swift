6bac6b7d6fc3d101623e7f59dd2d26543fa6939a
XanderB88 <alexxanderbykov@gmail.com>
2021-11-16 05:21:46 +0400

    Design a signUp screen

--- Messenger/Views/SignUpViewController.swift
+++ Messenger/Views/SignUpViewController.swift
@@ -1,49 +1,48 @@
 //
-//  LoginViewController.swift
+//  SignUpViewController.swift
 //  Messenger
 //
-//  Created by Alexander on 15.11.2021.
+//  Created by Alexander on 16.11.2021.
 //
 
 import UIKit
 
-class LogInViewController: UIViewController {
-    
+class SignUpViewController: UIViewController {
+
     // MARK: - Buttons
-    let googleButton = UIButton(title: "Google", backgroundColor: .mainWhite, backgroundColorAlfa: 0.2, icon: #imageLiteral(resourceName: "google"))
-    let loginButton = UIButton(title: "LogIn", backgroundColor: .mainWhite, backgroundColorAlfa: 0.2, icon: #imageLiteral(resourceName: "login"))
+    let signupButton = UIButton(title: "SignUp", backgroundColor: .mainWhite, backgroundColorAlfa: 0.2, icon: #imageLiteral(resourceName: "login"))
     
-    lazy var signupButton: UIButton = {
+    lazy var loginButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("SignUp", for: .normal)
+        button.setTitle("LogIn", for: .normal)
         button.setTitleColor(.mainRed, for: .normal)
         button.titleLabel?.font = .secondaryFont18
         return button
     }()
     
     // MARK: - Labels
-    let greetingLabel = UILabel(text: "WELCOME BACK!", textColor: .mainWhite, font: .headerFont30)
-    let googleLabel = UILabel(text: "LOGIN WITH", textColor: .secondaryWhite, font: .secondaryFont18)
-    let orLabel = UILabel(text: "OR", textColor: .secondaryWhite, font: .secondaryFont18)
+    let greetingLabel = UILabel(text: "GOOD TO SEE YOU!", textColor: .mainWhite, font: .headerFont30)
     let emailLabel = UILabel(text: "EMAIL", textColor: .secondaryWhite, font: .secondaryFont18)
     let passwordLabel = UILabel(text: "PASSWORD", textColor: .secondaryWhite, font: .secondaryFont18)
-    let needAccountLabel = UILabel(text: "NEED AN ACCOUNT?", textColor: .secondaryWhite, font: .secondaryFont18)
+    let confirmPasswordLabel = UILabel(text: "CONFIRM PASSWORD", textColor: .secondaryWhite, font: .secondaryFont18)
+    let alreadyOnboardLabel = UILabel(text: "ALREADY ONBOARD?", textColor: .secondaryWhite, font: .secondaryFont18)
     
     // MARK: - TextFields
     let emailTextField = UnderlineTextField( placeholder: "Email")
     let passwordTextField = UnderlineTextField( placeholder: "Password", isSecured: true)
+    let confirmPasswordTextField = UnderlineTextField( placeholder: "Confirm password", isSecured: true)
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.backgroundColor = .mainDark
         setupConstraints()
     }
 }
 
 // MARK: - Setup constraints
-extension LogInViewController {
+extension SignUpViewController {
     private func setupConstraints() {
         
         // Greeting label
@@ -56,32 +55,29 @@ extension LogInViewController {
             greetingLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
         
-        // Google button with label
-        googleLabel.translatesAutoresizingMaskIntoConstraints = false
-        googleButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        let googleButtonForm = UIStackView(arrangedSubviews: [googleLabel, googleButton], axis: .vertical, spacing: 15)
-        
         // Email textfield with label
         let emailTextFieldForm = UIStackView(arrangedSubviews: [emailLabel, emailTextField], axis: .vertical, spacing: 15)
         
         // Password textfield with label
         let passwordTextFieldForm = UIStackView(arrangedSubviews: [passwordLabel, passwordTextField], axis: .vertical, spacing: 15)
         
-        // Login form
-        let loginForm = UIStackView(arrangedSubviews: [googleButtonForm, orLabel, emailTextFieldForm, passwordTextFieldForm, loginButton], axis: .vertical, spacing: 40)
+        // Confirm password textfield with label
+        let confirmPasswordTextFieldForm = UIStackView(arrangedSubviews: [confirmPasswordLabel, confirmPasswordTextField], axis: .vertical, spacing: 15)
         
-        view.addSubview(loginForm)
+        // Signup form
+        let signupForm = UIStackView(arrangedSubviews: [emailTextFieldForm, passwordTextFieldForm, confirmPasswordTextFieldForm, signupButton], axis: .vertical, spacing: 40)
+        
+        view.addSubview(signupForm)
         
         NSLayoutConstraint.activate([
-            loginForm.topAnchor.constraint(equalTo: greetingLabel.bottomAnchor, constant: 100),
-            loginForm.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
-            loginForm.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+            signupForm.topAnchor.constraint(equalTo: greetingLabel.bottomAnchor, constant: 100),
+            signupForm.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            signupForm.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
         ])
         
         // Bottom form
         
-        let bottomForm = UIStackView(arrangedSubviews: [needAccountLabel, signupButton], axis: .horizontal, spacing: 5)
+        let bottomForm = UIStackView(arrangedSubviews: [alreadyOnboardLabel, loginButton], axis: .horizontal, spacing: 5)
         
         view.addSubview(bottomForm)
         
@@ -93,10 +89,11 @@ extension LogInViewController {
     }
 }
 
+
 // MARK: - Activate a canvas
 import SwiftUI
 
-struct LogInViewControllerProvider: PreviewProvider {
+struct SignUpViewControllerProvider: PreviewProvider {
     
     static var previews: some View {
         ContainerView().edgesIgnoringSafeArea(.all)
@@ -104,7 +101,7 @@ struct LogInViewControllerProvider: PreviewProvider {
     
     struct ContainerView: UIViewControllerRepresentable {
         
-        let viewController = LogInViewController()
+        let viewController = SignUpViewController()
         
         func makeUIViewController(context: Context) -> some UIViewController {
             return viewController
